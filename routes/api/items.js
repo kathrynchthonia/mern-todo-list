8398ca588fd7efae3eff6ac8888aa7cb8f9c27d0
twheelertech <tony@wheelertechconsulting.com>
2019-01-01 20:28:01 -0600

    finished backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -7,6 +7,30 @@ const Item = require("../../models/Item");
 //  @route GET api/items
 // @desc Get all items
 // @access Public
-router.get("/");
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
+
+//  @route POST api/items
+// @desc create an Item
+// @access Public
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+});
+
+//  @route DELETE api/items/:id
+// @desc DELETE an Item
+// @access Public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
 
 module.exports = router;
