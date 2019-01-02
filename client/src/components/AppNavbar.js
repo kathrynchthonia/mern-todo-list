19fab3cf1dc1dae5d064b1716ddd9d0b1eb263be
twheelertech <tony@wheelertechconsulting.com>
2019-01-01 23:42:23 -0600

    started react app

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Container
+} from "reactstrap";
+
+class AppNAvbar extends Component {
+  state = {
+    isOpen: false
+  };
+}
+
+toggle = () => {};
+
+export default AppNAvbar;
